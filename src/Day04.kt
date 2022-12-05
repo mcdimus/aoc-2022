5924df250eff31a23e500e3d4fcf351e087d76fb
Dmitri Maksimov <dmitri.maksimov@dynatrace.com>
2022-12-05 10:36:30 +0200

    Day04

---
+++ src/Day04.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    fun part1(input: List<String>) = parseInput(input)
+        .count { it.first contains it.second || it.second contains it.first }
+
+    fun part2(input: List<String>) = parseInput(input)
+        .count { it.first overlaps it.second }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun parseInput(input: List<String>) = input
+    .map { it.split(',') }
+    .map { it[0] to it[1] }
+    .map { it.first.toIntRange() to it.second.toIntRange() }
---
+++ src/IntRangeX.kt
@@ -0,0 +1,10 @@
+fun String.toIntRange(delimiter: Char = '-') = this.split(delimiter).let { it[0].toInt()..it[1].toInt() }
+
+infix fun IntRange.contains(other: IntRange): Boolean {
+    return this.contains(other.first) && this.contains(other.last)
+}
+
+infix fun IntRange.overlaps(other: IntRange): Boolean {
+    return this.contains(other.first) || this.contains(other.last)
+            || other.contains(this.first) || other.contains(this.last)
+}
